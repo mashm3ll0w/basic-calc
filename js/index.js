e34a6dc27c6436647cd186c989e271016b5a9df8
Charles Swaleh <charlswall@live.com>
2019-07-31 12:23:32 +0300

    add code to calculate BMI

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
-function add(number1, number2) {
-  return number1 + number2;
+// function add(number1, number2) {
+//   return number1 + number2;
+// }
+
+// var number1 = parseInt(prompt("Enter the First Number"));
+// var number2 = parseInt(prompt("Enter the Second Number"));
+
+// alert("The sum of your two numbers is " + add(number1, number2));
+
+function bmiCalculator(weight, height) {
+  var bmi = Math.round(weight / Math.pow(height, 2));
+  return (bmi);
 }
 
-var number1 = parseInt(prompt("Enter the First Number"));
-var number2 = parseInt(prompt("Enter the Second Number"));
+var weight = parseInt(prompt("Enter your Weight in Kgs"));
+var height = parseInt(prompt("Enter your Height in Meters"));
 
-alert("The sum of your two numbers is " + add(number1, number2));
+alert("Your BMI is " + bmiCalculator(weight, height));
