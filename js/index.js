40add563ce2c15e2de1a924afa854230518c75be
Charles Swaleh <charlswall@live.com>
2019-07-31 11:01:35 +0300

    add meaningful message to the result add

--- js/index.js
+++ js/index.js
@@ -5,4 +5,4 @@ function add(number1, number2) {
 var number1 = parseInt(prompt("Enter the First Number"));
 var number2 = parseInt(prompt("Enter the Second Number"));
 
-alert(add(number1, number2));
+alert("The sum of your two numbers is " + add(number1, number2));
