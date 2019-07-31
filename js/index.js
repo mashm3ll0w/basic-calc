7ccc0b1a76aacbcba65d97992ccee21b4bb87adb
Charles Swaleh <charlswall@live.com>
2019-07-31 10:49:40 +0300

    define the add function with prompts

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+var number1 = parseInt(prompt("Enter the First Number"));
+var number2 = parseInt(prompt("Enter the Second Number"));
+
+alert(add(number1, number2));
